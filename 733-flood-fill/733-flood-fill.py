c3c62d35339238aa3365f1e2b7c46eb811b32145
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-03 21:01:19 -0700

    Time: 186 ms (10.80%), Space: 14 MB (65.94%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,34 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        if image is None or image[sr][sc] == color:
+            return image
+        
+        
+        self.fill(image, sr, sc, image[sr][sc], color)
+        
+        
+        return image
+        
+        
+        
+        
+    def fill(self, image, r, c, pixel_to_change, color):
+    
+        # Checking if we go out of bounds
+        if r < 0 or r > len(image) - 1 or c > len(image[0]) - 1 or c < 0 or image[r][c] != pixel_to_change:
+            return
+        
+        image[r][c] = color
+        
+        # Down
+        self.fill(image, r + 1, c, pixel_to_change, color)
+        
+        # Up
+        self.fill(image, r - 1, c, pixel_to_change, color)
+        
+        # Left 
+        self.fill(image, r, c - 1, pixel_to_change, color)
+        
+        # Right
+        self.fill(image, r, c + 1, pixel_to_change, color)
