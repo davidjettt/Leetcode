4fa45142f86033731ffaf86ff2b7eb9cdde59fa3
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-06 01:21:14 -0700

    Time: 196 ms (81.92%), Space: 15.6 MB (34.10%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,21 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Time O(2n) Space O(n)
-        counts = {}
+#         counts = {}
         
-        for num in nums:
-            if num not in counts:
-                counts[num] = 1
-            else:
-                counts[num] += 1
+#         for num in nums:
+#             if num not in counts:
+#                 counts[num] = 1
+#             else:
+#                 counts[num] += 1
         
-        res = 0
-        majority = 0
+#         res = 0
+#         majority = 0
         
-        for num in counts:
-            if counts[num] > majority:
-                majority = counts[num]
-                res = num
-        return res
+#         for num in counts:
+#             if counts[num] > majority:
+#                 majority = counts[num]
+#                 res = num
+#         return res
 
+        count = {}
+        res, max_count = 0, 0
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+            res = num if count[num] > max_count else res
+            max_count = max(max_count, count[num])
+            
+        return res
+            
         
+
+            
