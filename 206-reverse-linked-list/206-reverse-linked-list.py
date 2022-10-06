e46ff316395c6e1727e32899caabe94959f35343
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-05 22:36:28 -0700

    Time: 75 ms (23.48%), Space: 20.4 MB (15.16%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,20 +5,6 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-#         prev = None
-#         curr = head
-        
-#         while curr:
-#             nxt = curr.next
-#             curr.next = prev
-#             prev = curr
-#             curr = nxt
-        
-#         return prev
-
-
-
         # need to keep track of prev, curr, curr.next
         # traverse through linked list while curr is truthy
         # save curr.next to a temp var
@@ -26,15 +12,46 @@ class Solution:
         # curr points to prev
         # prev points to temp var
         
-        prev = None
-        curr = head
+#         prev = None
+#         curr = head
         
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+#         while curr:
+#             temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = temp
             
-        return prev
+#         return prev
+        
+        # Time O(n)
+        # Space O(1)
+        
+        # RECURSIVE
+#         if not head:
+#             return None
+        
+#         newHead = head
+        
+#         if head.next:
+#             newHead = self.reverseList(head.next)
+#             head.next.next = head
+#         head.next = None
+        
+        # return newHead
+        
+        return self.reverse(head, None)
+    
+    def reverse(self, cur, prev):
+        if cur is None:
+            return prev
+        else:
+            next = cur.next
+            cur.next = prev
+            return self.reverse(next, cur)
+
+  
+            
+    
+        
         
         
