837784f25471c15ab377d28049c29b56dc91c5a8
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-07 18:30:00 -0700

    Time: 130 ms (5.16%), Space: 13.9 MB (20.32%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -18,26 +18,27 @@ class Solution:
         
 #         return image
         
+        return [ [x ^ 1 for x in reversed(row)] for row in image ]
         
-        for i in range(len(image)):
-            image[i].reverse()
+#         for i in range(len(image)):
+#             image[i].reverse()
             
             
-        row = 0
-        col = 0
-        row_length = len(image[0])
+#         row = 0
+#         col = 0
+#         row_length = len(image[0])
         
-        while row < len(image):
+#         while row < len(image):
             
-            if image[row][col] == 1:
-                image[row][col] = 0
-            else:
-                image[row][col] = 1
+#             if image[row][col] == 1:
+#                 image[row][col] = 0
+#             else:
+#                 image[row][col] = 1
                 
-            col += 1
+#             col += 1
             
-            if col == row_length:
-                col = 0
-                row += 1
+#             if col == row_length:
+#                 col = 0
+#                 row += 1
         
-        return image
+#         return image
