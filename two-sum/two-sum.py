6f3a61255f85a4fbd35c5c74525dee75d0d10930
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-09 22:10:27 -0700

    Time: 73 ms, Memory: 15.2 MB - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         lookup = {}
+        
+#         for index, num in enumerate(nums):
+#             if target - num in lookup:
+#                 return [index, lookup[target - num]]
+#             else:
+#                 lookup[num] = index
+        # for i in range(len(nums) - 1):
+        #     for j in range(len(nums)):
+        #         if j != i:
+        #             if nums[i] + nums[j] == target:
+        #                 return i, j
+        
+        
+        lookup = {}
+        
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [i, lookup[target - num]]
+            else:
+                lookup[num] = i
+                
+                
+        # Time O(n)
+        # Space O(n)
