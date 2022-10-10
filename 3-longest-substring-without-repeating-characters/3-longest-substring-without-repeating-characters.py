620880143fcaa7757bb821d8057ddb86f6af0b13
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-09 19:25:10 -0700

    Time: 98 ms (71.96%), Space: 14.1 MB (49.66%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,37 +5,34 @@ class Solution:
     
         # a b c a b c b b
         # l     r
-
-# max length is distance + 1 btwn left and right pointers  without repeating characters (r - l) + 1
-# iterate through string every time come accross c that is not repeating, count++ and add c to visited 
-# keep moving right pointer until come across c that is repeating
-# then need to set res to count if count > res, reset count, reset visited, and move left pointer plus 1
-# then do the same thing
-        
-#         pwwkew
-#         lr
         
         
-#         d a v d f 
-#           l   r
-
-  # a d v d f z y
-  # l   r  
+#         chars = set()
+#         res = 0
         
-#     b b b b b
-    
-#     l r
-        
-        chars = set()
-        res = 0
-        
-        l = 0
-        for r in range(len(s)):
-            while s[r] in chars:
-                chars.remove(s[l])
-                l += 1
-            chars.add(s[r])
+#         l = 0
+#         for r in range(len(s)):
+#             while s[r] in chars:
+#                 chars.remove(s[l])
+#                 l += 1
+#             chars.add(s[r])
                 
-            res = max(res, r - l + 1)
+#             res = max(res, r - l + 1)
             
+        # return res
+    
+    
+        l, r = 0, 0
+        res = 0
+        seen = set()
+        
+        while l < len(s) and r < len(s):
+            if s[r] not in seen:
+                seen.add(s[r])
+                r += 1
+                res = max(res, r - l)
+            else:
+                seen.remove(s[l])
+                l += 1
         return res
+        
