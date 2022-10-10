cc90c8d8d056d85cd56548f6bcf5c70a5778bb9f
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-09 22:06:54 -0700

    Time: 2503 ms (5.01%), Space: 28 MB (39.81%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,32 +2,26 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
         # edge case: if length of nums is 1 return nums[0]
-        
-#           0         4        8
-#         [-2,1,-3,4,-1,2,1,-5,4]
-#          lr   
-            
-#         res = nums[0]
-#         curr_sum = nums[0]
-#         curr_sum2 = 0
+
+
+        # BRUTE FORCE 
+#         max_sub = nums[0]
         
 #         for i in range(len(nums)):
-#             for j in range(len(nums)):
+#             curr_sum = 0
+#             for j in range(i, len(nums)):
                 
-#                 # print(nums[i], nums[j])
+#                 curr_sum += nums[j]
+#                 max_sub = max(max_sub, curr_sum)
+        
+#         return max_sub
                 
-#                 # curr_sum2 = 0
-#                 if curr_sum < curr_sum2 + nums[j]:
-#                     curr_sum2 += nums[j]
-#                     curr_sum = curr_sum2
-            
-#             curr_sum2 = 0
-            
-#         return curr_sum
                     
-                
+        # Time O(n)
+        # Space O(1)
+        
         max_sub = nums[0]
-        curr_sum = 0
+        curr_sum = 0 
         
         for n in nums:
             if curr_sum < 0:
