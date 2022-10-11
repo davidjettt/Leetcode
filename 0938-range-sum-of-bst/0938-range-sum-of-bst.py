331cf984d1d4b1bc7352c6d01be8757052f6f9be
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-10 17:21:22 -0700

    Time: 316 ms (71.93%), Space: 23 MB (50.20%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -8,20 +8,47 @@ class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         # need to find the sum of all nodes with values between low and high inclusive
         
+        if not root:
+            return 0
         res = 0
-        stack = [ root ]   # stack = FILO / LOFI
-        
-        while len(stack) > 0:
-            current = stack.pop()
-            
-            if low <= current.val <= high:
-                res += current.val
-                
-            if current.left:
-                stack.append(current.left)
-            if current.right:
-                stack.append(current.right)
-                
+        if low <= root.val <= high:
+            res += root.val
+        if root.val > low:
+            res += self.rangeSumBST(root.left, low, high)
+        if root.val < high:
+            res += self.rangeSumBST(root.right, low, high)
         return res
+#         sum_vals = 0
+#         left = self.rangeSumBST(root.left, low, high)
+#         right = self.rangeSumBST(root.right, low, high)
+        
+#         if low <= left <= high:
+#             sum_vals += left
+#         if low <= right <= high:
+#             sum_vals += right
+#         if low <= root.val <= high:
+#             sum_vals += root.val
+        
+#         return sum_vals
+        
+        
+#         res = 0
+#         stack = [ root ]   # stack = FILO / LOFI
+        
+#         while len(stack) > 0:
+#             current = stack.pop()
+            
+#             if low <= current.val <= high:
+#                 res += current.val
+                
+#             if current.left:
+#                 stack.append(current.left)
+#             if current.right:
+#                 stack.append(current.right)
+                
+#         return res
+    
+        # Time O(n)  # iterating through n nodes
+        # Space O(n)  # using stack like data structure
         
         
