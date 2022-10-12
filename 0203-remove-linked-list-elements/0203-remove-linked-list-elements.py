9fdba7a2f7223af92934b974883e8bbcf777ec3d
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-11 22:57:29 -0700

    Time: 164 ms (15.96%), Space: 17.9 MB (12.96%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -10,6 +10,9 @@ class Solution:
         # if curr.next.val == val, reassign curr.next = curr.next.next
         # if curr.next.next is None then node we want to remove is the last node so just say curr.next = None
         # return curr
+        
+        # Time O(n) traversing through n nodes
+        # Space O(1)
             
         dummy = ListNode()
         curr = head
@@ -17,8 +20,8 @@ class Solution:
         prev.next = curr
         
         while curr:
-            if curr.next == None and curr.val == val:
-                prev.next = None
+            # if curr.next == None and curr.val == val:
+            #     prev.next = None
             if curr.val == val:
                 new_curr = curr.next
                 prev.next = curr.next
@@ -27,25 +30,6 @@ class Solution:
             else:
                 prev = curr
                 curr = curr.next
-            
-    
-            
-        # while curr:
-        #     if curr.val == val and curr.next == None:
-        #         curr = None
-        #     elif curr.val == val:
-        #         new_curr = curr.next
-        #         curr.next = None
-        #         curr = new_curr
-        #     elif curr.next.next == None and curr.next.val == val:
-        #         curr.next = None
-        #         curr = curr.next
-        #     elif curr.next and curr.next.val == val: 
-        #         curr.next = curr.next.next
-        #         curr = curr.next
-        #     else:
-        #         curr = curr.next
-            # curr = curr.next
         
         return dummy.next
             
