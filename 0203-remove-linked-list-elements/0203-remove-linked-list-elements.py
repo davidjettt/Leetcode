553ed47b9e4959f9ac3fdfcb7e5ebd72b8aeaf57
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-11 23:12:34 -0700

    Time: 118 ms (59.96%), Space: 17.8 MB (38.87%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -6,27 +6,37 @@
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
-        # traverse through list
-        # if curr.next.val == val, reassign curr.next = curr.next.next
-        # if curr.next.next is None then node we want to remove is the last node so just say curr.next = None
-        # return curr
-        
         # Time O(n) traversing through n nodes
         # Space O(1)
             
-        dummy = ListNode()
-        curr = head
-        prev = dummy
-        prev.next = curr
+#         dummy = ListNode()
+#         curr = head
+#         prev = dummy
+#         prev.next = curr
+        
+#         while curr:
+#             new_curr = curr.next
+#             if curr.val == val:
+#                 prev.next = curr.next
+#             else:
+#                 prev = curr
+            
+#             curr = new_curr
+        
+#         return dummy.next
+        
+        
+        prev, curr = None, head
         
         while curr:
-            new_curr = curr.next
             if curr.val == val:
-                prev.next = curr.next
+                if prev:
+                    prev.next = curr.next
+                else:
+                    head = curr.next
+                
+                curr = curr.next
             else:
-                prev = curr
-            
-            curr = new_curr
+                prev, curr = curr, curr.next
         
-        return dummy.next
-            
+        return head
