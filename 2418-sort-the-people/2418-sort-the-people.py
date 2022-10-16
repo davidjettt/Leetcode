3e3ef0359ef34e1b3ca0c9b24a35cc7e209bae0f
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-15 17:11:35 -0700

    Time: 223 ms (70.70%), Space: 14.5 MB (46.69%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,34 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-#         [ Mary, John, Emma ]
-
-#         [ 180, 165, 170 ]
-
-#         {
-#             180: Mary,
-#             165: John,
-#             170: Emma
-#         }
         
-#         lookup = {}
-        
-#         for i in range(len(heights)):
-            
-#             if heights[i] not in lookup:
-#                 heights[i] = names[i]
-        
-        res = []
-        data = zip(heights, names)
+        # data = zip(heights, names)
         # data.sort(reverse=True)
-        test = sorted(data, reverse=True)
+        test = sorted(zip(heights,names), reverse=True)
         
         return [ n for h, n in test]
         
-#         for h, n in test:
-#             res.append(n)
-            
-#         return res
     
 #         x, y = zip(*sorted(zip(heights, names), reverse=True))
         
