cfcbcdf86098ad1422f4b384e63fa75c9f223b5e
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-19 13:23:26 -0700

    Time: 345 ms (25.57%), Space: 16.7 MB (49.71%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,18 +2,30 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
         # needs to be Time O(n) Space O(1)
+        # length of nums will always be odd
+        # every num appears twice execpt for one
+        
+        # what i know
+            # need to look at every single num once to determine if there is a duplicate
+            
+        n = len(nums)    
+        r = nums[0]
+        
+        for i in range(1, n):
+            r = r ^ nums[i]
+            
+        return r
         
         
-        count = {}
+#         count = {}
         
-        for n in nums:
-            if n not in count:
-                count[n] = 1
-            else:
-                count[n] += 1
+#         for n in nums:
+#             if n not in count:
+#                 count[n] = 1
+#             else:
+#                 count[n] += 1
                 
         
-        for k,v in count.items():
-            if v == 1:
-                return k
-            
+#         for k,v in count.items():
+#             if v == 1:
+#                 return k
