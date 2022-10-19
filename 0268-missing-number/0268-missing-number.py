c3fda8aa1f036ae0344f1828d55ed214a7390c57
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-19 16:10:59 -0700

    Time: 280 ms (56.85%), Space: 15.2 MB (80.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,19 +1,39 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+
         
-        # if len(nums) == 1:
-        #     if nums[0] == 1:
-        #         return 0
+#         allNums = []
         
-        allNums = []
-        
-        for i in range(1, len(nums) + 1):
-            allNums.append(i)
+#         for i in range(1, len(nums) + 1):
+#             allNums.append(i)
             
         
-        for n in allNums:
-            if n not in nums:
-                return n
+#         for n in allNums:
+#             if n not in nums:
+#                 return n
         
-        return 0
+#         return 0
             
+    
+    # initial missingNum variable set to 0
+    # sort the nums array
+    # [ 0, 1, 3 ]
+    # loop through nums array
+    # if n is not equal to missingNum
+        # misssingNum ++
+        # go to next iteration
+    # else return missingNum
+    
+    # [ 0, 1 ]
+    
+        missingNum = 0 
+        nums.sort()
+
+        for n in nums:
+            if n == missingNum:
+                missingNum += 1
+            else:
+                return missingNum
+                # missingNum += 1
+        return missingNum
+    
