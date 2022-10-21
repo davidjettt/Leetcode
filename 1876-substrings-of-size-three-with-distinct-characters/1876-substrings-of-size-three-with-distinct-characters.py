cfa852a78282a2d969ed8b278f2592852e8d6e5e
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-20 17:28:07 -0700

    Time: 47 ms (76.84%), Space: 13.9 MB (59.91%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        
+        # init two pointers windowStart = 0, windowEnd = 2
+        # init count variable
+        
+        # use while loop as long as windowEnd < len(s)
+            
+            # init chars = empty set
+            # put s[windowStart], s[windowEnd], s[windowStart + 1] in the set
+            
+            # if length of set is not 3 then add windowStart++ and windowEnd++ and then move on to next iteration/substring
+            # else count++
+            
+        
+        # return count
+        
+        
+        
+        if len(s) < 3:
+            return 0
+        
+        windowStart = 0
+        windowEnd = 2
+        count = 0
+        
+        while windowEnd < len(s):
+            chars = set()
+            
+            chars.add(s[windowStart])
+            chars.add(s[windowStart + 1])
+            chars.add(s[windowEnd])
+            
+            if len(chars) != 3:
+                windowStart += 1
+                windowEnd += 1
+            else:
+                count += 1
+                windowStart += 1
+                windowEnd += 1
+                
+                
+        return count
