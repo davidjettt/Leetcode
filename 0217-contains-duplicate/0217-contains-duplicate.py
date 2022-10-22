bf733999c8db23a869197e8df6fe8f2ba68c3dd5
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-21 23:36:52 -0700

    Time: 1483 ms (5.02%), Space: 25.6 MB (97.94%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,39 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Time O(n)
+        # Space O(n)
+#         nums2 = set(nums)
+        
+#         if len(nums2) < len(nums):
+#             return True
+#         else:
+#             return False
+
+        
+        nums.sort()
+        windowStart = 0
+        for windowEnd in range(1, len(nums)):
+            if nums[windowStart] == nums[windowEnd]:
+                return True
+            else:
+                windowStart += 1
+                
+        return False
+            
+        
+
+#  Time O(n)
+# Space O(1)
+#         unique = set()
+        
+#         for num in nums:
+            
+#             if num in unique:
+#                 return True
+            
+#             else:
+#                 unique.add(num)
+        
+#         return False
+            
+            
