1db25eeb31b1470f0ec5a390449452382f25009a
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-24 00:23:00 -0700

    Time: 57 ms (87.10%), Space: 14.5 MB (67.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        s_sort = sorted(s)
-        t_sort = sorted(t)
+#         s_sort = sorted(s)
+#         t_sort = sorted(t)
         
-        return True if s_sort == t_sort else False
+#         return True if s_sort == t_sort else False
+    
+        hashmap = {}
         
+        for c in s:
+            if c in hashmap:
+                hashmap[c] += 1
+            else:
+                hashmap[c] = 1
+        
+        for c in t:
+                
+            if c in hashmap:
+                hashmap[c] -= 1
+            else:
+                return False
+            
+            if hashmap[c] == 0:
+                del hashmap[c]
+        
+        return len(hashmap) == 0
