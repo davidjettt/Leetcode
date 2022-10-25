3ecdefe64b8de256a743a1dca2421f5e2892b3b1
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-25 00:53:58 -0700

    Time: 200 ms (74.87%), Space: 13.9 MB (57.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,27 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
         frequency_map = {}
-        
         res = 0 
-        # left, right = 0, 0
         left = 0
         
         for right in range(len(s)):
             char = s[right]
-            if char in frequency_map:
-                frequency_map[char] += 1
-            else:
-                frequency_map[char] = 1
+            frequency_map[char] = 1 + frequency_map.get(char, 0)
     
             most_frequent_char = max(frequency_map, key=frequency_map.get)
-            window_length = right - left + 1
             
-            if window_length - frequency_map[most_frequent_char] <= k:
-                res = right - left + 1
-            else:
+            if right - left + 1 - frequency_map[most_frequent_char] > k:
                 frequency_map[s[left]] -= 1
                 left += 1
+            
+            res = max(res, right - left + 1)
+            
+            # if window_length - frequency_map[most_frequent_char] <= k:
+            #     res = right - left + 1
+            # else:
+            #     frequency_map[s[left]] -= 1
+            #     left += 1
                 # most_frequent_char = max(frequency_map, key=frequency_map.get)
                 # if right - left + 1 - frequency_map[most_frequent_char] <= k:
                 #     res = right - left + 1
