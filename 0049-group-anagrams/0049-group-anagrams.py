aecd7cfc84c279ab23cc4c1c48fb7ae40802096f
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-25 23:52:49 -0700

    Time: 255 ms (27.03%), Space: 19.9 MB (10.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,11 +4,13 @@ class Solution:
         # res = {}
         
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26   # a.....z  index 0 is a; index 25 is z
             
             for c in s:
+                # ascii value of current char - ascii value of 'a' gives correct index of char in count 
                 count[ord(c) - ord('a')] += 1
             
+            # need to change to a tuple bc python doesn't allow lists to be values of dictionary
             res[tuple(count)].append(s)
             
         return res.values()
