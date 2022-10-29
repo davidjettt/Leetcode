10601778bc926aaa9cb387a70371ded88476d511
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-28 23:06:48 -0700

    Time: 111 ms (55.42%), Space: 13.9 MB (61.74%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,41 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        
+        prefix_sum = [arr[0]]
+        
+        for i in range(1, len(arr)):
+            prefix_sum.append(prefix_sum[-1] + arr[i])
+            
+        res = 0
+        
+        for start in range(len(arr)):
+            end = start
+            while end < len(arr):
+                if start == 0:
+                    res += prefix_sum[end]
+                else:
+                    res += prefix_sum[end] - prefix_sum[start - 1]
+                
+                end += 2
+        
+        return res
+    
+        
+    
+#         res = 0
+#         if len(arr) % 2 != 0:
+#             res = sum(arr)
+            
+        
+#         left = 0
+        
+#         for right in range(len(arr)):
+#             res += arr[right]
+            
+#             while (right - left + 1) % 2 != 0 and left < right:
+#                 res += arr[left]
+#                 left += 1
+            
+#             res += arr[left]
+        
+#         return res
