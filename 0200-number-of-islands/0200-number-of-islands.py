6aa24542f2f8ed3cd9a757fac05316433e1260f3
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-10-31 23:11:38 -0700

    Time: 614 ms (65.99%), Space: 28.9 MB (5.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,46 +5,56 @@ class Solution:
         count = 0
         rows, cols = len(grid), len(grid[0])
         
+        def bfs(r, c):
+            visited.add((r, c))
+            q = collections.deque()
+            q.append((r, c))
+            
+            while len(q) > 0:
+                curr = q.popleft()
+
+                if grid[curr[0]][curr[1]] == '0':
+                    continue
+
+                dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+                for dir in dirs:
+                    new_row, new_col = curr[0] + dir[0], curr[1] + dir[1]
+                    # check if within bounds
+                    if new_row >= 0 and new_row <= len(grid) - 1 and new_col >= 0 and new_col <= len(grid[0]) - 1:
+                        if (new_row, new_col) not in visited:
+                            visited.add((new_row, new_col))
+                            q.append((new_row, new_col))
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
                     count += 1
-                    visited.add((r, c))
-                    q = deque()
-                    q.append((r, c))
+                    # visited.add((r, c))
+                    # q = collections.deque()
+                    # q.append((r, c))
                     
-                    while len(q) > 0:
-                        curr = q.popleft()
+#                     while len(q) > 0:
+#                         curr = q.popleft()
                         
-                        if grid[curr[0]][curr[1]] == '0':
-                            continue
+#                         if grid[curr[0]][curr[1]] == '0':
+#                             continue
                             
-                        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+#                         dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-                        for dir in dirs:
-                            new_row, new_col = curr[0] + dir[0], curr[1] + dir[1]
-                            # check if within bounds
-                            if new_row >= 0 and new_row <= len(grid) - 1 and new_col >= 0 and new_col <= len(grid[0]) - 1:
-                                if (new_row, new_col) not in visited:
-                                    visited.add((new_row, new_col))
-                                    q.append((new_row, new_col))
+#                         for dir in dirs:
+#                             new_row, new_col = curr[0] + dir[0], curr[1] + dir[1]
+#                             # check if within bounds
+#                             if new_row >= 0 and new_row <= len(grid) - 1 and new_col >= 0 and new_col <= len(grid[0]) - 1:
+#                                 if (new_row, new_col) not in visited:
+#                                     visited.add((new_row, new_col))
+#                                     q.append((new_row, new_col))
         
         return count
                         
             
-            
-    
-#         def getNeighbors(grid, row, col):
-#             neighbors = []
-#             dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-#             for dir in dirs:
-#                 new_row, new_col = row + dir[0], col + dir[1]
-#                 # check if within bounds
-#                 if new_row >= 0 and new_row <= len(grid) - 1 and new_col >= 0 and new_col <= len(grid[0]) - 1:
-#                     if (new_row, new_col) not in visited:
-#                         neigbors.append((new_row, new_col))
-#             return neighbors
+        
     
 #     def getNeighbors(self, grid, row, col):
 #         res = []
