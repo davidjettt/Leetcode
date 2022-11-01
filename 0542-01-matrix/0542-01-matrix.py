af54b329a7010399ff35847ece58ea0d6ce30d6e
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-01 01:09:39 -0700

    Time: 811 ms (83.23%), Space: 17.3 MB (52.59%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,9 +1,7 @@
-
 class Solution:
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         visited = set()
-        from collections import deque
-        q = deque()
+        q = collections.deque()
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
@@ -14,7 +12,7 @@ class Solution:
             x,y = q.popleft()
             for dirr in [(1,0), (-1,0), (0,1), (0,-1)]:
                 newX, newY = x+dirr[0], y+dirr[1]
-                if newX >= 0 and newX <= len(matrix)-1 and newY >= 0 and newY <= len(matrix[0])-1 and (newX, newY) not in visited:
+                if 0 <= newX < len(matrix) and 0 <= newY < len(matrix[0]) and (newX, newY) not in visited:
                         matrix[newX][newY] = matrix[x][y] + 1
                         visited.add((newX, newY))
                         q.append((newX, newY))
