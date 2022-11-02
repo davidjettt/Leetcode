b2799bd189a04ca978edb112dc7922dda7e43d3d
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-02 01:23:31 -0700

    Time: 142 ms (53.00%), Space: 14.2 MB (95.92%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -22,32 +22,9 @@ class Solution:
                 stack.append(b * a)
             elif c =='/':
                 a, b = stack.pop(), stack.pop()
-                stack.append(int(b / a))
+                stack.append(math.trunc(b / a))
             else:
                 stack.append(int(c))
         
-        # for i in range(len(tokens)):
-        #     if tokens[i].isnumeric():
-        #         stack.append(int(tokens[i]))
-        #     else:
-        #         # v2 = stack.pop()
-        #         # v1 = None
-        #         # if len(stack) == 0:
-        #         #     v1 = 0
-        #         # else:
-        #         #     v1 = stack.pop()
-        #         # print(stack)
-        #         v2 = stack.pop()
-        #         v1 = stack.pop()
-        #         if tokens[i] == '+':
-        #             # computation = v1 + v2
-        #             stack.append(v1 + v2)
-        #         elif tokens[i] == '-':
-        #             stack.append(v1 - v2)
-        #         elif tokens[i] == '*':
-        #             stack.append(v1 * v2)
-        #         else:
-        #             # stack.append(int(v1 / v2))
-        #             stack.append(int(float(v1) / v2))
                 
         return stack[0]
