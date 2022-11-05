491a9256341a5d75bf35140bf0856a069c5644ac
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-05 01:22:20 -0700

    Time: 1206 ms (44.44%), Space: 15.1 MB (81.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,13 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # have pointer start at beginning and end of array
-        # if the value at the index before end is greater than 1,
-        # that means it is possible to reach the end
-        # so if that is true shift the end pointer to the left by 1 (decrement)
-        # else short circuit and return false
-        # and then do the same comparison
-        # if right pointer meets left pointer then true
-        
+        # Time O(n) where n is length of input array
+        # Space O(1)
         goal = len(nums) - 1
         
         for i in range(len(nums) - 1, -1, -1):
