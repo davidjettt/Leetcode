c8a573fb5d0efbf39147db7f6dfd72623746bbe0
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-12 18:55:34 -0800

    Time: 1209 ms (23.36%), Space: 25.9 MB (68.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,20 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        vals = set()
+        
+        for n in nums:
+            if n in vals:
+                return True
+            else:
+                vals.add(n)
+        return False
+        
+        
+        
+        
+        
+        
+        
         # Time O(n)
         # Space O(n)
 #         nums2 = set(nums)
@@ -10,15 +25,15 @@ class Solution:
 #             return False
 
         
-        nums.sort()
-        windowStart = 0
-        for windowEnd in range(1, len(nums)):
-            if nums[windowStart] == nums[windowEnd]:
-                return True
-            else:
-                windowStart += 1
+#         nums.sort()
+#         windowStart = 0
+#         for windowEnd in range(1, len(nums)):
+#             if nums[windowStart] == nums[windowEnd]:
+#                 return True
+#             else:
+#                 windowStart += 1
                 
-        return False
+#         return False
             
         
 
