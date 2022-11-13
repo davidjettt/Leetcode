194b6e80f32b71277785de261ee6d03d62ca655c
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-12 21:23:04 -0800

    Time: 2632 ms (38.76%), Space: 25 MB (86.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        # [1,2,3,2,5,7,2,4,9,0,9]
-        #  l                 r
-        
+        # Time O(n)
+        # Space O(1)
         max_profit = 0
         
         low, high = 0, 1
@@ -13,48 +11,12 @@ class Solution:
             
             if prices[high] > prices[low]:
                 max_profit = max(max_profit, curr_profit)
-            elif curr_profit < 0:
+            else:
                 low = high
             high += 1
         
         return max_profit
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # have array of prices of a stock with index = day
-        # need to return max profit between 2 prices in the array
-        # must buy first and then sell
-        # Buying index has to be lower than selling index
-        # if not profit possible aka array with decreasing values then return 0
-        
-        # day = 1
         
 #         left = 0 #buy
 #         right = 1  # sell
@@ -71,26 +33,3 @@ class Solution:
 #             right += 1
             
 #         return max_profit
-
-        
-#         left = 0
-#         right = 1
-#         max_profit = 0
-        
-#         while right < len(prices):
-            
-#             current_profit = prices[right] - prices[left]
-            
-#             if current_profit > max_profit:
-#                 max_profit = current_profit
-            
-#             elif current_profit < 0:
-#                 left = right
-            
-#             right += 1
-                
-#         return max_profit
-            
-        
-        # Time O(n)
-        # Space O(1)
