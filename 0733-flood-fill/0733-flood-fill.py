4eaa2fbc219c4c66bc025e23d1fca5e94dab7d73
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-13 01:01:47 -0800

    Time: 186 ms (24.65%), Space: 14 MB (67.04%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,32 +1,76 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
-        # Checks if image exists or if current pixel is already the same color 
-        if image is None or image[sr][sc] == color:
+        # dfs
+        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]) or image[sr][sc] == color:
             return image
         
-        self.fill(image, sr, sc, image[sr][sc], color)
+        self.dfs(image, sr, sc, image[sr][sc] , color)
         return image
         
         
-    def fill(self, image, r, c, initial_pixel, color):
-        # Checking if we go out of bounds or if current pixel we are on doesn't need to be changed (don't want to change the 0s)
-        if r < 0 or r > len(image) - 1 or c > len(image[0]) - 1 or c < 0 or image[r][c] != initial_pixel:
-            return
         
+    def dfs(self, image, r, c, initial, color):
+        if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != initial:
+            return 
+            
         image[r][c] = color
         
-        # Down
-        self.fill(image, r + 1, c, initial_pixel, color)
+        self.dfs(image, r + 1, c, initial, color)
+        self.dfs(image, r - 1, c, initial, color)
+        self.dfs(image, r, c + 1, initial, color)
+        self.dfs(image, r, c - 1, initial, color)
+
         
-        # Up
-        self.fill(image, r - 1, c, initial_pixel, color)
         
-        # Left 
-        self.fill(image, r, c - 1, initial_pixel, color)
         
-        # Right
-        self.fill(image, r, c + 1, initial_pixel, color)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Checks if image exists or if current pixel is already the same color 
+#         if image is None or image[sr][sc] == color:
+#             return image
+        
+#         self.fill(image, sr, sc, image[sr][sc], color)
+#         return image
+        
+        
+#     def fill(self, image, r, c, initial_pixel, color):
+#         # Checking if we go out of bounds or if current pixel we are on doesn't need to be changed (don't want to change the 0s)
+#         if r < 0 or r > len(image) - 1 or c > len(image[0]) - 1 or c < 0 or image[r][c] != initial_pixel:
+#             return
+        
+#         image[r][c] = color
+        
+#         # Down
+#         self.fill(image, r + 1, c, initial_pixel, color)
+        
+#         # Up
+#         self.fill(image, r - 1, c, initial_pixel, color)
+        
+#         # Left 
+#         self.fill(image, r, c - 1, initial_pixel, color)
+        
+#         # Right
+#         self.fill(image, r, c + 1, initial_pixel, color)
         
     # Time O(n)
     # Space O(n)
