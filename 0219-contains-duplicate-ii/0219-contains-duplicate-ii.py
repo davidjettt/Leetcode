da4f387631fac76b57c6620aa5d6fb4744d54abd
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-12 20:05:57 -0800

    Time: 1624 ms (34.12%), Space: 25.5 MB (92.10%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,26 +1,22 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         
-        # nums at the indicies need to be the same
-        # absolute value of i - j needs to be less than or equal to k
-        # indicies can't be the same
+        i, j = 0, 0
+        seen = set()
         
-        # init windowStart = 0 windowEnd = 1
-        
-        # while windowEnd < len(nums)
-            # if abs(windowStart - windowEnd) <= k
-                # if nums[windowStart] == nums[windowEnd]
-                    # return True
-                # else windowEnd++
+        while j < len(nums):
+            if abs(i - j) > k:
+                seen.remove(nums[i])
+                i += 1
             
-            # if abs(windowStart - windowEnd) > k
-                # move windowStart plus 1
-                
+            if nums[j] not in seen:
+                seen.add(nums[j])
+            else:
+                return True
+            j += 1
+        return False
         
 #         if len(nums) < 2:
-#             return False
-        
-#         if k == 0:
 #             return False
         
 #         windowStart, windowEnd = 0, 1
@@ -34,31 +30,5 @@ class Solution:
 #                     windowEnd += 1
 #             else:
 #                 windowStart += 1
-        
-#         return False
-        i=0
-        j=0
-        seen = set()
-        while j<len(nums):
-            if j-i>k:
-                seen.remove(nums[i])
-                i+=1
-                
-            if nums[j] not in seen:
-                seen.add(nums[j])
-            else: return True
-                    
-            j+=1    
-        return False
-    
-    
-#         hmap = {}
-        
-#         for i in range(len(nums)):
-#             if nums[i] not in hmap:
-#                 hmap[nums[i]] = i
-#             else:
-#                 if abs(i - nums[i]) <= k:
-#                     return True
         
 #         return False
