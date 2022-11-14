1f7b0bdcdf1f8520e2f8822e350ff64efa85abca
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-13 17:47:55 -0800

    Time: 73 ms (81.98%), Space: 14.1 MB (94.14%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,25 +1,5 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-                
-        # iterate through magazine and store each character in list
-        # iterate through ransomNote and check if character is in list
-        # if true then remove character from list
-        # return false if character isn't in list
-        # if able to iterate through entire string then return false
-        
-        
-#         chars_bank = [ char for char in magazine ]
-        
-        
-#         for char in ransomNote:
-            
-#             if char in chars_bank:
-#                 chars_bank.remove(char)
-#             else:
-#                 return False
-        
-#         return True
-    
         chars_bank = {}
         
         for char in magazine:
@@ -38,6 +18,6 @@ class Solution:
             
         
         
-        # Time O(2n^2) for each char in ransomNote we are performing .remove
+        # Time O(n + m) where n is the size ransomNote and m is size of magazine
         
         # Space O(n) where n is size of chars_bank
