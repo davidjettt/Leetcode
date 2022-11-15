de4d74d5ec8408bff15bc19fc5bd26bf496101fe
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-14 23:52:08 -0800

    Time: 275 ms (89.56%), Space: 27.1 MB (45.52%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,30 +1,27 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        # basically need to find minimum length subarray where the numbers add up to be equal to or greater than target
-        # cant sort array because that changes position of each element
-        
         # SUBARRAY SUM MUST BE GREATER THAN OR EQUAL
         
         # [2,3,1,2,4,3]
         #    l   r
         
-        i, j = 0, 0
+        l, r = 0, 0
         result = float('inf')
         window_sum = 0
-        while j < len(nums):
-            window_sum += nums[j]
+        while r < len(nums):
+            window_sum += nums[r]
             
-            # if window_sum >= target:
-                
             while window_sum >= target:
-                result = min(result, j - i + 1)
-                window_sum -= nums[i]
-                i += 1
+                result = min(result, r - l + 1)
+                window_sum -= nums[l]
+                l += 1
+                
+                if result == 1:
+                    return 1
             
-            j += 1
-            
-            
+            r += 1
         return 0 if result == float('inf') else result
+    
     
 #         # BRUTE FORCE SOLUTION
 #         shortest = float('inf')
