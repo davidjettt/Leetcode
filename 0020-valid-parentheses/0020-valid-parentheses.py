c15efcadbff19cd9b3b08074a59cd4c628600863
David Jetsupphasuk <102331121+davidjettt@users.noreply.github.com>
2022-11-15 22:33:56 -0800

    Time: 36 ms (88.01%), Space: 13.9 MB (26.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        
-        # init a stack
-        # create the hashmap
-        
-        # iterate through the string
-            # if the character is in the hashmap
-                # pop off from the stack
-                #  check the popped off character to see if it is the right pair (use hashmap to check)
-                # if not a matching pair return False
-            
-            # else
-                # add character into the stack
-                
-                
-        # if length of stack is > 0
-            # return true
-        # else return false
-          
+        # Time O(n) n is the size input s
+        # Space O(n)
         pairs = {
             ')': '(',
             '}': '{',
@@ -33,35 +16,11 @@ class Solution:
                 
                 if ele != pairs[char]:
                     return False
-                
-                    
             else:
                 stack.append(char)
         
         return True if not len(stack) else False
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         # Time O(n)
         # Space O(n)
         
@@ -88,27 +47,4 @@ class Solution:
 #         return True
         
         
-#         chars = {
-#             ')': '(',
-#             ']': '[',
-#             '}': '{'
-#         }
-
-#         stack = []
-
-
-#         for char in s:
-#             if char in chars:
-#                 if stack and stack[-1] == chars[char]:
-#                     stack.pop()
-#                 else:
-#                     return False
-#             else:
-#                 stack.append(char)
-
-
-#         if len(stack):
-#             return False
-
-#         return True
     
