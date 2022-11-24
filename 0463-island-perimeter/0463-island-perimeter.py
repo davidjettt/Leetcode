e1b89c7ee7c1251dc94731d9aa3fb04af07ad3d9
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-23 23:21:26 -0800

    Time: 1958 ms (10.51%), Space: 22.3 MB (21.45%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -18,15 +18,15 @@ class Solution:
         
         # return dfs when it is finished
         
-        # Time O(n)
-        # Space O(n)
-        visited = set() #{ (0, 1), (1, 1), (2,1), (3,1), (3,0), (1,2) }
-        
-        
+        # Time O(n * m)
+        # Space O(n * m)
+        rows, cols = len(grid), len(grid[0])
+        visited = set() 
+    
         def dfs(r, c):
             if (r, c) in visited:
                 return 0
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
                 return 1
             
             visited.add((r, c))
@@ -40,79 +40,17 @@ class Solution:
             return perimeter
                 
         
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
                     return dfs(r, c)
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        # [
-        #     [0,1,0,0],
-        #     [1,1,1,0],
-        #     [0,1,0,0],
-        #     [1,1,0,0]
-        # ]
-        # if the 1 is adjacent to a 0 or out of bounds that counts as 1 side
-        # total amount of adjacent 0s and out of bounds is the total perimeter of 1 square
-        # if current node is a 1, check its neighbors
-        # if its neighbors is out of bounds or a 0 then add 1 to result perimeter
-        # but only add neighbors that are 1s to q
         
         # Time O(n * m) only visited each postion in the grid once
         # Space O(n * m) have visit set and recursive calls
 #         rows, cols = len(grid), len(grid[0])
 #         visited = set()
-#         # stack = []
-        
 #         def dfs(r, c):
 #             if 0 > r or r >= rows or 0 > c or c >= cols or grid[r][c] == 0:
 #                 return 1
@@ -127,25 +65,6 @@ class Solution:
             
 #             return perim
             
-# #         def dfs(r, c):
-# #             res = 0
-# #             visited.add((r, c))
-# #             stack.append((r, c))
-# #             while stack:
-# #                 row, col = stack.pop()
-                
-# #                 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-                
-# #                 for dirX, dirY in directions:
-# #                     newX, newY = row + dirX, col + dirY
-
-# #                     if 0 <= newX < rows and 0 <= newY < cols and (newX, newY) not in visited and grid[newX][newY] == 1:
-# #                         visited.add((newX, newY))
-# #                         stack.append((newX, newY))
-# #                     else:
-# #                         res += 1
-            
-# #             return res
         
 #         for r in range(rows):
 #             for c in range(cols):
