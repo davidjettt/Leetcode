7f6f3b3b309a6fee5a897c1c46445a17876e7c43
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-25 22:24:00 -0800

    Time: 112 ms (45.31%), Space: 13.9 MB (49.17%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,85 @@
+import collections
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        '''
+        
+        [
+            [2,1,1],
+            [1,1,1],
+            [0,1,2]
+        ]
+        
+        [
+            [2,2,2],
+            [2,2,0],
+            [0,2,2]
+        ]
+        
+        
+        [
+            [2,1,1],
+            [0,1,1],
+            [1,0,1]
+        ]
+        
+        
+        iterate through the grid
+        run bfs on every 2 we find
+        
+        bfs
+            pop off from queue
+            * might need to do something here *
+            check for neighbors that are 1s
+            before add neighbors to queue, increment our result by 1
+            add to our visited set
+            
+        1) have a check for 1s in first loop (like an else if) 
+        2) after done w/ bfs, loop through grid again looking for 1s
+        '''
+        
+        res = [0] # 1
+        rows, cols = len(grid), len(grid[0])
+        visited = set() 
+        q = collections.deque() # [  (2,2, 4)  ]
+        
+        def bfs():
+            
+            while q:
+                r, c, mins = q.popleft()  # (2,1,3)
+                
+                if grid[r][c] == 1:
+                    grid[r][c] = 2
+                
+                
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                for dirX, dirY in directions:
+                    newX, newY = dirX + r, dirY + c
+                    
+                    # if 0 <= newX < rows and 0 <= newY < cols and (newX, newY) not in visited and grid[newX][newY] == 2:
+                    #     mins -= 1
+                    #     visited.add((newX, newY))
+                    
+                    if 0 <= newX < rows and 0 <= newY < cols and (newX, newY) not in visited and grid[newX][newY] == 1:
+                        visited.add((newX, newY))
+                        q.append((newX, newY, mins + 1))
+                        # res += 1
+                if len(q) == 0:
+                    res[0] = mins
+            
+            
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2 and (r, c) not in visited:
+                    q.append((r, c, 0))
+                    visited.add((r, c))
+                    
+        bfs()
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    return -1
+        return res[0]        
+        
+        
