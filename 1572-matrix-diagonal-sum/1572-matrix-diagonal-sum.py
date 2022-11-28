50ab21d05e8854afe6fe7d1c94dc5cc161d5080f
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-28 00:31:34 -0800

    Time: 283 ms (21.63%), Space: 14.1 MB (93.70%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -63,3 +63,28 @@ class Solution:
             c -= 1
             
         return primary + secondary
+        
+#         n = len(mat)
+        
+#         mid = n // 2
+        
+#         summation = 0
+        
+#         for i in range(n):
+            
+#             # primary diagonal
+#             summation += mat[i][i]
+            
+#             # secondary diagonal
+#             summation += mat[n-1-i][i]
+            
+            
+#         if n % 2 == 1:
+#             # remove center element (repeated) on odd side-length case
+#             summation -= mat[mid][mid]
+            
+            
+#         return summation
+        
+        
+        
