22ab782810fcd243f6a9c356b92cfeb4f63483bd
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-28 01:08:34 -0800

    Time: 39 ms (87.36%), Space: 13.9 MB (15.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -43,87 +43,8 @@ class Solution:
                 mid += 1
             else:
                 mid += 1
-        
-            # if nums[low] == 0:
-            #     low += 1
-            
-            # mid += 1
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
         
     
-#         mid, low, high = 0, 0, len(nums) - 1
         
-#         while mid <= high:
-            
-#             if nums[mid] == 2:
-#                 nums[high], nums[mid] = nums[mid], nums[high]
-                
-#                 high -= 1
-            
-#             elif nums[mid] == 0:
-#                 nums[low], nums[mid] = nums[mid], nums[low]
-                
-#                 low += 1
-#                 mid += 1
-            
-#             else:
-#                 mid += 1
-                
