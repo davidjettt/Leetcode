d463f627ac36ed72727aea7cabd13ad87c3cefa0
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-27 22:15:24 -0800

    Time: 450 ms (86.54%), Space: 28.9 MB (62.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -32,33 +32,76 @@ class Solution:
         res= 3
         [ 1, 3, 6, 7, 10, 11, 12 ] 3
                        l       r
-        
+        [ '.',  '.', 2, 3, '.', 5, '.', '.' ]
+                               lr
         '''
         
+        reference = set(nums)
         
-        unique = list(set(nums))
+        res = 0
         
-        unique.sort()
+        for n in reference:
+            count = 1
+            if n - 1 not in reference:
+                while n + 1 in reference:
+                    count += 1
+                    n = n + 1
+                res = max(res, count)
         
-        if len(unique) == 1:
-            return 1
-        if len(unique) == 0:
-            return 0
-        
-        l, r = 0, 1
-        
-        res = 1
-        
-        while r < len(unique):
-            if unique[r] - 1 == unique[r - 1]:
-                res = max(res, r - l + 1)
-                
-            else:
-                l = r
-            
-            r += 1
-            
         return res
+            
+            
+                
+        
+#         biggest = max(nums)
+        
+#         num_line = [ ['.'] for i in range(biggest + 1)  ] 
+        
+#         for n in set(nums):
+#             num_line[n] = n
+            
+            
+#         res = 1
+#         l, r = 0, 1
+        
+#         while r < len(num_line):
+#             while num_line[r] == '.' and r < len(num_line):
+#                 r += 1
+#                 l = r
+            
+#             if num_line[r - 1] == '.':
+#                 r += 1
+#                 continue
+            
+#             if num_line[r] - 1 == num_line[r - 1]: 
+#                 res = max(res, r - l + 1)
+#             r += 1
+                
+#         return res
+        
+        
+        # Time O(nlogn) where n is the size of the unqiue array
+        # Space O(n) where n is the size of the set
+#         unique = list(set(nums))
+        
+#         unique.sort()
+        
+#         if len(unique) == 1:
+#             return 1
+#         if len(unique) == 0:
+#             return 0
+        
+#         l, r = 0, 1
+#         res = 1
+        
+#         while r < len(unique):
+#             if unique[r] - 1 == unique[r - 1]:
+#                 res = max(res, r - l + 1)
+#             else:
+#                 l = r
+#             r += 1
+            
+#         return res
         
         
         
