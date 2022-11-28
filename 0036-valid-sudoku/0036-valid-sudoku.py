cc778d7a6d08f87d2969fd8774c66ea3b1befe39
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-27 23:34:35 -0800

    Time: 122 ms (78.52%), Space: 14 MB (35.30%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,82 +1,89 @@
+import collections
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         '''
+         {
+            0: set(),
+         }
+        
         {
-            'r1': {5, 3, 7}
+            (0 , 1 ):
         }
         
-        board[r][0]
         
-        if c == 0:
-            add num to col set
+        iterate through the entire board
+            check if value at the current cell already exists in hashmaps
+            
+                if true, return false
+            
+            add that value to the hashsets
         
+        return true
         
-        if board[r][0]
-        
-        
-        set up the hashmap 
-        
-        iterate through the board
-        
-        at each row position add num to hashmap for corresponding key value
-        also check for duplicate
-        
-        same for cols
-        
-        0-2, 3-5, 6-8 for x and y
-        
-        
+        Time O(9^2)
+        Space O(n)
         
         '''
-        cols = collections.defaultdict(set)
+        
         rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
         
         for r in range(9):
             for c in range(9):
-                if board[r][c] == '.': 
+                value = board[r][c]
+                if value == '.':
                     continue
-                if board[r][c] in cols[c] or board[r][c] in rows[r] or board[r][c] in squares[(r // 3, c // 3)]:
+                if value in rows[r] or value in cols[c] or value in squares[(r // 3, c // 3)]:
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c //3)].add(board[r][c])
                 
+                rows[r].add(value)
+                cols[c].add(value)
+                squares[(r // 3, c // 3)].add(value)
+        
         return True
         
-#         rows = {
-#             0: set(),
-#             1: set(),
-#             2: set(),
-#             3: set(),
-#             4: set(),
-#             5: set(),
-#             6: set(),
-#             7: set(),
-#             8: set()
-#         }
-#         columns = {
-#             0: set(),
-#             1: set(),
-#             2: set(),
-#             3: set(),
-#             4: set(),
-#             5: set(),
-#             6: set(),
-#             7: set(),
-#             8: set()
-#         }
-#         i = -1
         
-#         for r in range(9):
-#             i += 1
-#             if board[r][i] in columns[r]:
-#                 return False
-#             columns[r].add(board[r][i])
-#             for c in range(9):
-#                 if board[r][c] in rows[c]:
-#                     return False
-#                 rows[c].add(board[r][c])
-                
         
-#         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
