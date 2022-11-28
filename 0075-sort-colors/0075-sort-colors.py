f496100f39d4632032375d11392f34ace2195c46
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-28 01:07:55 -0800

    Time: 67 ms (39.90%), Space: 13.9 MB (64.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,127 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-    
-        mid, low, high = 0, 0, len(nums) - 1
+        '''
+        
+        [0,0,1,1,2,2]
+             l m  h
+        
+        [0,0,0,1,1,2,2,2]
+             l       mh  
+             
+             
+        [0, 1, 2]
+           lh  m   
+           
+        [ 0, 1, 2 ]
+            lh m 
+           
+           
+        [0, 1, 2, 1, 0, 2, 1, 2] 
+        l   m              h
+        
+        looping as long as mid < high
+        
+            if mid points to 2: swap with what high is pointing to and move high
+            if mid points to a 0: swap with what low is pointing to and move low
+            do nothing when mid points to 1
+        
+        '''
+        
+        low, mid, high = 0, 0, len(nums) - 1
+        
         
         while mid <= high:
-            
             if nums[mid] == 2:
-                nums[high], nums[mid] = nums[mid], nums[high]
-                
+                nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
-            
             elif nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                
+                nums[mid], nums[low] = nums[low], nums[mid]
                 low += 1
                 mid += 1
-            
             else:
                 mid += 1
+        
+            # if nums[low] == 0:
+            #     low += 1
+            
+            # mid += 1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+#         mid, low, high = 0, 0, len(nums) - 1
+        
+#         while mid <= high:
+            
+#             if nums[mid] == 2:
+#                 nums[high], nums[mid] = nums[mid], nums[high]
+                
+#                 high -= 1
+            
+#             elif nums[mid] == 0:
+#                 nums[low], nums[mid] = nums[mid], nums[low]
+                
+#                 low += 1
+#                 mid += 1
+            
+#             else:
+#                 mid += 1
                 
