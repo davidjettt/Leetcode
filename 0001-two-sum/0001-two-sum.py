4ef7e3c28499d505deaf99c0d82430357330f24e
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-29 01:52:18 -0800

    Time: 62 ms (95.01%), Space: 15.2 MB (26.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,28 +1,42 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         lookup = {}
-        
-#         for index, num in enumerate(nums):
-#             if target - num in lookup:
-#                 return [index, lookup[target - num]]
-#             else:
-#                 lookup[num] = index
-
-        for i in range(len(nums) - 1):
-            for j in range(len(nums)):
-                if j != i:
-                    if nums[i] + nums[j] == target:
-                        return i, j
         
         # Time O(n)
         # Space O(n)
-#         lookup = {}
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [i, lookup[target - num]]
+            else:
+                lookup[num] = i
+                
+
+    
+        # Time O(nlogn)
+        # Space O(1)
+#         nums.sort()
         
-#         for i, num in enumerate(nums):
-#             if target - num in lookup:
-#                 return [i, lookup[target - num]]
+#         l, r = 0, len(nums) - 1
+        
+#         while l < r:
+#             curr_sum = nums[l] + nums[r]
+            
+#             if curr_sum < target:
+#                 l += 1
+#             elif curr_sum > target:
+#                 r -= 1
 #             else:
-#                 lookup[num] = i
+#                 return [l, r]
+
+
+        # Brute Force Approach
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+                
+        
+
                 
     
             
