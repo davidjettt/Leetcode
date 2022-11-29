c68e979704ee9e40e95e8f5249c304f563865bfe
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-28 23:00:43 -0800

    Time: 2131 ms (5.00%), Space: 27.3 MB (55.63%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,95 +1,5 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        '''
-        
-        [ 1, 2, 3, 1 ]
-                   i
-                   
-        [ 1, 0, 1, 1 ]
-                   i
-    
-        {
-          1: 0
-        }
-         0   1. 2. 3. 4. 5
-        [ 1, 2, 3, 1, 2, 3 ] k = 2
-          i                 
-        
-        iterate through the array
-        
-        check if number is in hashmap
-            check if abs(i - j) <= k
-            if true, return true
-            if false, replace old idx with new idx in hashmap
-        
-        storing num and index as key: value pair
-        
-        return true
-        
-        Time O(n)
-        Space O(n)
-        '''
-        
-        unique = {}
-        
-        for i in range(len(nums)):
-            if nums[i] in unique:
-                if abs(i - unique[nums[i]]) <= k:
-                    return True
-                else:
-                    unique[nums[i]] = i
-            
-            else:
-                unique[nums[i]] = i
-    
-        return False
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 #         i, j = 0, 0
 #         seen = set()
@@ -106,14 +16,14 @@ class Solution:
 #             j += 1
 #         return False
     
-        # hashmap = {}
+        hashmap = {}
 
-#         for idx, val in enumerate(nums):
-#             if val in hashmap and abs(idx - hashmap[val]) <= k:
-#                 return True
+        for idx, val in enumerate(nums):
+            if val in hashmap and abs(idx - hashmap[val]) <= k:
+                return True
 
-#             hashmap[val] = idx
-#         return False
+            hashmap[val] = idx
+        return False
 
                     
         
