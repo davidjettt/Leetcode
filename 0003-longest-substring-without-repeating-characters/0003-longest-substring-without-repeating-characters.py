c968b56434ec346e627d335e0942bead98852927
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-29 20:29:26 -0800

    Time: 1079 ms (11.15%), Space: 14.2 MB (5.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,5 +1,118 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+        {
+            'a': 3,
+            'b': 4,
+            'c': 5
+        }
+        
+        { b,c } 
+        res = 1 
+        a b c a b c b b
+                  l r 
+         
+          
+    
+    
+        iterate through string using left and right pointers
+        
+            check if s[r] is in set
+                if true, then move left pointer until s[l] == s[r] (plus 1)
+                if false, add to set and compute and compare substring size with result
+                
+        
+        return res
+        
+        Time O(n) where n is the length of the string
+        Space O(n) where n is the size of the set
+        '''
+        res = 0
+        chars = set()
+        # chars = {}
+        
+        l, r = 0, 0
+
+        while r < len(s):
+            print(chars)
+            if s[r] in chars:
+                while s[l] != s[r] and l < r:
+                    chars.remove(s[l])  
+                    # del chars[s[l]]
+                    l += 1
+                
+                l += 1
+            else:
+                chars.add(s[r])
+                # chars[s[r]] = r
+                res = max(res, r - l + 1) 
+            
+            r += 1
+                
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         # need to find length of longest substring without repeating characters in the answer
     
@@ -7,32 +120,32 @@ class Solution:
         # l     r
         
         
-        chars = set()
-        res = 0
-        
-        l = 0
-        for r in range(len(s)):
-            while s[r] in chars:
-                chars.remove(s[l])
-                l += 1
-            chars.add(s[r])
-                
-            res = max(res, r - l + 1)
-            
-        return res
-    
-    
-#         l, r = 0, 0
+#         chars = set()
 #         res = 0
-#         seen = set()
         
-#         while l < len(s) and r < len(s):
-#             if s[r] not in seen:
-#                 seen.add(s[r])
-#                 res = max(res, r - l + 1)
-#                 r += 1
-#             else:
-#                 seen.remove(s[l])
+#         l = 0
+#         for r in range(len(s)):
+#             while s[r] in chars:
+#                 chars.remove(s[l])
 #                 l += 1
+#             chars.add(s[r])
+                
+#             res = max(res, r - l + 1)
+            
 #         return res
+    
+    
+        l, r = 0, 0
+        res = 0
+        seen = set()
+        
+        while l < len(s) and r < len(s):
+            if s[r] not in seen:
+                seen.add(s[r])
+                res = max(res, r - l + 1)
+                r += 1
+            else:
+                seen.remove(s[l])
+                l += 1
+        return res
         
