5426df23d9e61cba583a637be002db5aed32cde9
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-11-30 02:17:35 -0800

    Time: 141 ms (72.31%), Space: 14 MB (31.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -19,35 +19,50 @@ class Solution:
                 
         
         e i d b a o o o
-              l   r
+          l r
         
         
         
         
         
         '''
-        
-        
-        
+        # Time O(n * m) where n is the length of s2 and m is the length of s1
+        # Space O(n * m) where n is the size of chars and m is the size of chars2
+        if len(s1) > len(s2): return False
         
         l, r = 0, len(s1) - 1
-        chars = {}
-        for c in s1:
-            chars[c] = 1 + chars.get(c, 0)
+        s1_chars = {}
+        s2_chars = {}
         
-        while r < len(s2):
-            chars2 = {}
+        for i in range(len(s1)):
+            s1_chars[s1[i]] = 1 + s1_chars.get(s1[i], 0)
+            s2_chars[s2[i]] = 1 + s2_chars.get(s2[i], 0)
             
-            for i in range(l, r + 1):
-                chars2[s2[i]] = 1 + chars2.get(s2[i], 0)
-                
-            if chars2 == chars:
+        while r < len(s2):
+            if s2_chars == s1_chars:
                 return True
-            # if sorted(s1) == sorted(s2[l:r+1]):
-            #     return True
+            
+            if s2_chars[s2[l]] == 1: 
+                s2_chars.pop(s2[l])
+            else:
+                s2_chars[s2[l]] -= 1
             
             l += 1
             r += 1
-        
+            if r < len(s2):
+                s2_chars[s2[r]] = 1 + s2_chars.get(s2[r], 0)
         return False
+            
+        
+        
+#         while r < len(s2):
+#             chars2 = {}
+#             for i in range(l, r + 1):
+#                 chars2[s2[i]] = 1 + chars2.get(s2[i], 0) 
+#             if chars2 == s1_chars:
+#                 return True
+#             l += 1
+#             r += 1
+        
+#         return False
             
