5e1a7dccc95f9a7cb59e5af7a69e08fcff16d9ec
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-04 12:22:58 -0800

    Time: 794 ms (12.51%), Space: 15 MB (19.51%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -0,0 +1,56 @@
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        '''
+        
+        [
+            [2,0,0,1],
+            [0,3,1,0],
+            [0,5,2,0],
+            [4,0,0,2]
+        ]
+        
+        [
+            [5,7,0],
+            [0,3,1],
+            [0,5,0]
+        ]
+        
+        
+        check diagonals for non zeros (if find 0 return false), can do same logic for even and odd length grids
+        also mark positions as visited
+        
+        check all other cells for zeros (if find non zero return false)
+        
+        '''
+        
+        visited = set()
+        
+        r, c = 0, 0
+        # primary diagonal
+        while r < len(grid) and c < len(grid):
+            if (r, c) not in visited:
+                visited.add((r, c))
+                if grid[r][c] == 0:
+                    return False
+            
+            r += 1
+            c += 1
+            
+        # secondary diagonal
+        r, c = 0, len(grid) - 1
+        
+        while r < len(grid) and c >= 0:
+            if (r, c) not in visited:
+                visited.add((r, c))
+                if grid[r][c] == 0:
+                    return False
+            
+            r += 1
+            c -= 1
+        
+        for r in range(len(grid)):
+            for c in range(len(grid)):
+                if (r, c) not in visited and grid[r][c] != 0:
+                    return False
+        return True
+        
