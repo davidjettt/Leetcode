f1e30bfe8064ae1416c9ee32f31961ded43eae6a
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-04 12:01:09 -0800

    Time: 611 ms (63.83%), Space: 16.7 MB (19.50%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,21 +3,19 @@ class Solution:
         
         # [1, 2, 3, 4, 5] k=7 output: 1
         #              l r
-        
-        # [-1, -1, 1] k=0
-        # l    r
-        # [-1, -2, -1]
-        
-        res = 0
-        curr_sum = 0
+   
+        # Time O(n) where n is size of nums
+        # Space O(n) where n is size of prefix_sums hashmap
+        res = 0 # 2
+        curr_sum = 0 # 3
         prefix_sums = {0: 1}
         
         for n in nums:
             curr_sum += n
-            diff = curr_sum - k
-            
+            diff = curr_sum - k # 1
+            # if diff exists in hashmap then that means we have valid subarrays with sum equal to k
             res += prefix_sums.get(diff, 0)
-            
+            # Increment number of prefix sums with curr_sum value
             prefix_sums[curr_sum] = 1 + prefix_sums.get(curr_sum, 0)
         
         return res
