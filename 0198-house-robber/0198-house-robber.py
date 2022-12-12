d7b72ad26cde7a04fcc789a4bf0c313bbfdc1282
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-12 00:05:40 -0800

    Time: 47 ms (72.39%), Space: 14 MB (20.36%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+         0 1 2 3
+        [1,2,3,1]
+
+         0 1 2 3 4
+        [2,7,9,3,1]
+
+            0(1)
+            /\
+         2(3)  3(1)
+         /      /
+
+         [2,1,1,2]
+        '''
+        for i in range(1,len(nums)):
+            if i == 1:
+                nums[i] = max(nums[i], nums[i - 1])
+            else:
+                nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
+        return nums[-1]
