4491c80374f62067ab76d9fc489b70434532aaeb
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-12 00:23:48 -0800

    Time: 50 ms (69.14%), Space: 13.9 MB (66.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -14,9 +14,18 @@ class Solution:
 
          [2,1,1,2]
         '''
-        for i in range(1,len(nums)):
-            if i == 1:
-                nums[i] = max(nums[i], nums[i - 1])
-            else:
-                nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
-        return nums[-1]
+        # for i in range(1,len(nums)):
+        #     if i == 1:
+        #         nums[i] = max(nums[i], nums[i - 1])
+        #     else:
+        #         nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
+        # return nums[-1]
+        
+        house1, house2 = 0, 0
+        
+        for curr_house in nums:
+            temp = max(curr_house + house1, house2)
+            house1 = house2
+            house2 = temp
+        return house2
+    
