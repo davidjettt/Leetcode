dd5f5b4a7d90dd2ebacd1f4de6839c21491ce776
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-13 12:39:09 -0800

    Time: 184 ms (9.18%), Space: 40.7 MB (5.10%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,48 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        '''
+
+        can make three bucket sort arrays and iterate through all three of them at the same time to form new string
+
+        first need to make a frequency map
+        make bucket sort array (length of arr will be 62 b/c lower and capital letters and digits)
+            where idx represents number of times letter appears
+
+
+        tree
+
+        123Aaabc      aa123Abc
+
+
+        []
+        '''
+        freq_map = {}
+        lowercase = [[] for _ in range(len(s) + 1)]
+        uppercase = [[] for _ in range(len(s) + 1)]
+        digits = [[] for _ in range(len(s) + 1)]
+
+        for char in s:
+            freq_map[char] = 1 + freq_map.get(char, 0)
+
+        for char, count in freq_map.items():
+            if char.isnumeric():
+                digits[count].append(char)
+            elif char.isupper():
+                uppercase[count].append(char)
+            else:
+                lowercase[count].append(char)
+        
+        i = len(s)
+        res = ''
+        while i >= 0:
+            for x in lowercase[i]:
+                res += (x * i)
+            for y in uppercase[i]:
+                res += (y * i)
+            for z in digits[i]:
+                res += (z * i)
+            i -= 1
+        return res
+            
+
+
