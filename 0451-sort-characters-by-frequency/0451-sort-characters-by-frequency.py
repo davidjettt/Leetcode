c9d5e4887167973a5f8191b7e81e9595e3d02ea2
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-13 13:07:23 -0800

    Time: 93 ms (42.23%), Space: 23.7 MB (8.12%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,21 +1,27 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         '''
-
-        can make three bucket sort arrays and iterate through all three of them at the same time to form new string
-
-        first need to make a frequency map
-        make bucket sort array (length of arr will be 62 b/c lower and capital letters and digits)
-            where idx represents number of times letter appears
-
-
-        tree
-
-        123Aaabc      aa123Abc
-
-
-        []
         '''
+        # Time O(n)
+        # Space O(n)
+        freq_map = {}
+        buckets = [[] for _ in range(len(s) + 1)]
+        
+        for char in s:
+            freq_map[char] = 1 + freq_map.get(char, 0)
+
+        for char, count in freq_map.items():
+            buckets[count].append(char)
+
+        res = []
+        for i in range(len(buckets) - 1, -1, -1):
+            for char in buckets[i]:
+                # res += (char * i)
+                res.append(char * i)
+        return ''.join(res)
+        
+        
+        
         # Time O(nlogn)
         # Space O(n)
 #         freq_map = {}
@@ -30,22 +36,7 @@ class Solution:
         
 #         return res
 
-        freq_map = {}
-        buckets = [[] for _ in range(len(s) + 1)]
 
-
-        for char in s:
-            freq_map[char] = 1 + freq_map.get(char, 0)
-
-        for char, count in freq_map.items():
-            buckets[count].append(char)
-
-        res = []
-        for i in range(len(buckets) - 1, -1, -1):
-            for char in buckets[i]:
-                # res += (char * i)
-                res.append(char * i)
-        return ''.join(res)
 
             
 
