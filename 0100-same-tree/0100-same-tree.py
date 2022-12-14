1ba3c50f61a716224a45739d0f587822b095c165
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-13 16:31:35 -0800

    Time: 41 ms (77.44%), Space: 13.9 MB (29.82%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,31 +6,41 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
         
-        x = []
-        y = []
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
         
-        def dfs(node, check):
-            if not node:
-                if check == 'p':
-                    x.append(None)
-                    return
-                else:
-                    y.append(None)
-                    return
-            
-            if check == 'p':
-                x.append(node.val) 
-            else:
-                y.append(node.val)
+    
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         # Time O(n + m)
+#         # Space O(n + m)
+#         x = []
+#         y = []
+        
+#         def dfs(node, check):
+#             if not node:
+#                 if check == 'p':
+#                     x.append(None)
+#                     return
+#                 else:
+#                     y.append(None)
+#                     return
+#             if check == 'p':
+#                 x.append(node.val) 
+#             else:
+#                 y.append(node.val)
                 
-            dfs(node.left, check)
-            dfs(node.right, check)
+#             dfs(node.left, check)
+#             dfs(node.right, check)
                 
-        
-
-        dfs(p, 'p')
-        dfs(q, 'q')
-        return x == y
+#         dfs(p, 'p')
+#         dfs(q, 'q')
+#         return x == y
         
         
