0e2df0f4661081c42fc0a8ec34a2ef21cf679f48
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-15 00:54:51 -0800

    Time: 196 ms (90.26%), Space: 13.9 MB (91.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -6,6 +6,25 @@ class Solution:
         2 2 1
         1 2 2
         '''
+        # Initialize a list of zeros with a length of the target amount + 1
+        dp = [0] * (amount + 1)
+
+        # The number of ways to make up 0 dollars is 1
+        dp[0] = 1
+
+        # Iterate over the coins
+        for coin in coins:
+            # Iterate over the amounts from 1 to the target amount
+            for i in range(1, amount + 1):
+                # If the current coin is less than or equal to the current amount, we can use it to make up the amount
+                if coin <= i:
+                    # The number of ways to make up the current amount is equal to the number of ways to make up the
+                    # remaining amount (i.e. the current amount minus the value of the current coin) plus the number of ways
+                    # to make up the current amount without using the current coin
+                    dp[i] = dp[i] + dp[i - coin]
+
+        # Return the number of ways to make up the target amount
+        return dp[amount]
         
         memo = {}
 
