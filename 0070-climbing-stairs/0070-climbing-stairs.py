7332fceb25fcfcd9130fa47b84280cc9831da9a5
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-12-16 09:43:23 -0800

    Time: 30 ms (92.88%), Space: 13.9 MB (58.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,47 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        one, two = 1, 1
-        
-        for i in range(n - 1):
-            
-            temp = one
-            one = one + two
-            two = temp
-            
-        return one
-        
-        
+        '''
+                             
+            4                       
+           /\              
+          3  2                    
+         /\  /\                  
+         2 1 1 0                  
+         /\ 
+         1 0
+        /
+        0
+
+
+        '''
+        # DP Bottom-Up solution without extra memory b/c we can just shift the one and two variables
         # Time O(n)
         # Space O(1)
+        # one, two = 1, 1
+        # for _ in range(n - 1):
+        #     tmp = one
+        #     one = one + two
+        #     two = tmp
+        # return one
+
+        # DFS Memoization 
+        # Time O(n)
+        # Space O(n)
+        memo = {}
+
+        def dfs(step):
+            if step in memo:
+                return memo[step]
+            if step > n:
+                return 0
+            if step == n:
+                return 1
+            
+            s1 = dfs(step + 1)
+            # memo[step + 1] = s1
+            s2 = dfs(step + 2)
+            # memo[step + 2] = s2
+            memo[step] = s1 + s2
+            return s1 + s2
+        
+        return dfs(1) + dfs(2)
